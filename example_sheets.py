6fa5766f7c1bde88545bd5404674406e0545bcc1
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-26 21:26:46 +0300

    Added  processing schedule

--- example_sheets.py
+++ example_sheets.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import sheets_api
+import sending_ok
 
 
 def main():
@@ -10,7 +11,16 @@ def main():
     client = sheets_api.get_client()
     records = sheets_api.get_all_records(client, spreadsheet_id)
     posts = sheets_api.filter_posts_by_status(records, sheets_api.STATUS_PENDING)
-    print(posts)
+    # print(posts)
+
+    for post in posts:
+        if 'OK' in post.get('platform'):
+            status = publish_post_ok(post)
+            print(status)
+        if 'TG' in post.get('platform'):
+            print('TG')
+        if 'VK' in post.get('platform'):
+            print('VK')
     row = 3
     sheets_api.update_post_status(
         client,
