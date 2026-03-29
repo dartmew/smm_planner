9e5fd4038cad71d20115786ff9e5bf25c2766d06
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-29 15:06:24 +0300

    Update sending_ok.py

--- sending_ok.py
+++ sending_ok.py
@@ -1,7 +1,5 @@
 import requests
 import hashlib
-import random
-import time
 from environs import Env
 
 
@@ -15,10 +13,10 @@ def upload_foto_and_get_id(group_id, app_id, access_token, app_secret_key, img_u
     signature_data = ''.join([f'{k}={v}' for k, v in sorted(upload_url_params.items())])
     signature_data += app_secret_key
     upload_url_params['sig'] = hashlib.md5(signature_data.encode('utf-8')).hexdigest()
-    upload_url_params['access_token'] = access_token   
+    upload_url_params['access_token'] = access_token
     response = requests.post('https://api.ok.ru/fb.do', data=upload_url_params).json()
     upload_url = response['upload_url']
-    # photo = 
+    # photo =
     upload_response = requests.post(upload_url, files=photo).json()
     photo_id = upload_response['photo_id']
     return photo_id
@@ -30,10 +28,16 @@ def publish_post_ok(pending):
     app_id = env.str('OK_APP_ID')
     app_secret_key = env.str('OK_APP_SECRET_KEY')
     access_token = env.str('OK_ACCESS_TOKEN')
-    group_id = env.str('OK_GROUP_ID')    
+    group_id = env.str('OK_GROUP_ID')
     message = pending.get('text')
     img_url = pending.get('media_link')
-    photo_id = upload_foto_and_get_id(group_id, app_id, access_token, app_secret_key, img_url)
+    photo_id = upload_foto_and_get_id(
+        group_id,
+        app_id,
+        access_token,
+        app_secret_key,
+        img_url
+    )
     params = {
         'application_key': app_id,
         'method': 'mediatopic.post',
@@ -42,13 +46,13 @@ def publish_post_ok(pending):
         "text": message,
         'format': 'json',
         'attachment': {
-            'media': [ 
-                {"type": "photo", "list": [{ "id": photo_id}]},
+            'media': [
+                {"type": "photo", "list": [{"id": photo_id}]},
             ]
         }
-    }    
+    }
     signature_data = ''.join([f'{k}={v}' for k, v in sorted(params.items())])
-    signature_data += app_secret_key   
+    signature_data += app_secret_key
     params['sig'] = hashlib.md5(signature_data.encode('utf-8')).hexdigest()
     params['access_token'] = access_token
     response = requests.post('https://api.ok.ru/fb.do', params=params)
