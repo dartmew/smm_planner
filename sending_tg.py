49829fd6180ba95f2c5398b58facf9d527c94e03
Vitaliy <vitalianobro@gmail.com>
2026-03-29 17:54:47 +0300

    debug scripts

--- sending_tg.py
+++ sending_tg.py
@@ -11,26 +11,25 @@ CHAT_ID = env.str('TELEGRAM_CHAT_ID')
 
 
 def sending_post_in_tg(post):
-	bot = telebot.TeleBot(token=TOKEN)	
-	send = bot.send_photo(
-		chat_id=CHAT_ID,
-		photo=f'https://drive.google.com/uc?export=download&id={extract_google_drive_id(post['media_link'])}',
-		caption=post['text']
-	)
-	with open('posts_ids.json', 'r+') as file:
-		ids = json.load(file)
-		ids[post['id']]=send.message_id
-		json.dump(ids, open('posts_ids.json','w+'))
-	
+    bot = telebot.TeleBot(token=TOKEN)
+    send = bot.send_photo(
+        chat_id=CHAT_ID,
+        photo=f'https://drive.google.com/uc?export=download&id={extract_google_drive_id(post['media_link'])}',
+        caption=post['text'])
+    with open('posts_ids.json', 'r+') as file:
+        ids = json.load(file)
+        ids[post['id']] = send.message_id
+        json.dump(ids, open('posts_ids.json', 'w+'))
+
 
 def delete_post_in_tg(post_id):
-	bot = telebot.TeleBot(token=TOKEN)
-	with open('posts_ids.json', 'r+') as file:
-		ids = json.load(file)
-		id_for_delete = ids[post_id]
-		result = bot.delete_messages(
-			chat_id=CHAT_ID,  
-			message_ids=[id_for_delete]
-		)
-		if result:
-			del ids[post_id]
+    bot = telebot.TeleBot(token=TOKEN)
+    with open('posts_ids.json', 'r+') as file:
+        ids = json.load(file)
+        id_for_delete = ids[post_id]
+        result = bot.delete_messages(
+            chat_id=CHAT_ID,
+            message_ids=[id_for_delete])
+        if result:
+            del ids[post_id]
+            json.dump(ids, open('posts_ids.json', 'w+'))
